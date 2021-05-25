00d81d133ffe139e8b038bc4029f489e37c11be1
Marcin Bober <mbober1@gmail.com>
2021-05-25 12:22:13 +0200

    comunication update

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,20 +1,38 @@
 #include "fan.hpp"
 #include "freertos/task.h"
 #include "driver/uart.h"
+#include <string>
 
 const gpio_num_t tachoPin = GPIO_NUM_34;
 const gpio_num_t pwmPin = GPIO_NUM_32;
 const ledc_channel_t pwmChannel = LEDC_CHANNEL_0;
 const pcnt_unit_t pcntUnit = PCNT_UNIT_0;
 
-#define BUF_SIZE (1024)
 const uart_port_t uart = UART_NUM_0; 
+
+void uartRX();
 
 extern "C" void app_main() {
     Fan fan(tachoPin, pwmPin, pwmChannel, pcntUnit);
 
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-    // uint8_t data[] = {'d','u','p', 'a', '\n','\0'};
+    // uartRX();
+
+    while (true)
+    {
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        uint speed = fan.getSpeed();
+        printf("C%d;G%d;S%d;T%d;", speed, speed, 69, 96);
+    }
+    
+}
+
+
+
+
+
+void uartRX() {
+    const int bufferSize = 1024;
+    uint8_t data[bufferSize];
 
     uart_config_t uart_config;
     uart_config.baud_rate = 115200;
@@ -24,19 +42,57 @@ extern "C" void app_main() {
     uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
     uart_config.source_clk = UART_SCLK_APB;
 
-    // We won't use a buffer for sending data.
-    uart_driver_install(uart, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_driver_install(uart, 1024 * 2, 0, 0, NULL, 0);
     uart_param_config(uart, &uart_config);
-    // uart_set_pin(uart, 35, 34, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    int length;
+    static std::string bufferString;
+    int separator;
+
 
     while (true)
     {
-        int len = uart_read_bytes(uart, data, BUF_SIZE, 20 / portTICK_RATE_MS);
-        if(len > 0) printf("dupa\n");
-        // uart_write_bytes(uart, (const char *) data, len);
+        uart_get_buffered_data_len(uart, (size_t*)&length);
+        length = uart_read_bytes(uart, data, length, 20 / portTICK_RATE_MS);
+
+        if(length > 0) {
+            std::string dataString((char*)data, length);
+            printf("Len: %d -> Data: %s\n", length, dataString.c_str());
+            bufferString.append(dataString);
+        }
+
+        separator = bufferString.find(';');
+        while(separator != std::string::npos) {
+            std::string parse = bufferString.substr(0, separator);
+            bufferString.erase(0, separator + 1);
+            separator = bufferString.find(';');
+
+            printf("Parsed: %s\n", parse.c_str());
+
+            switch (parse.at(0))
+            {
+                case 'P': { // set pwm1
+                    parse.erase(0, 1);
+                    int pwm = std::atoi(parse.c_str());
+                    printf("PWM1 Speed: %d\n", pwm);
+                    break;
+                }
+
+                case 'W': { // set pwm2
+                    parse.erase(0, 1);
+                    int pwm = std::atoi(parse.c_str());
+                    printf("PWM2 Speed: %d\n", pwm);
+                    break;
+                }
+                
+                default: {
+                    printf("Sparsowane smieci: %s\n", parse.c_str());
+                    break;
+                }
+            }
+        }
+
         vTaskDelay(100 / portTICK_PERIOD_MS);
-        uint speed = fan.getSpeed();
-        printf("C%d;G%d;S%d;T%d;\n", speed, speed, 69, 96);
     }
     
 }
--- Qt/src/serial.cpp
+++ Qt/src/serial.cpp
@@ -7,7 +7,6 @@
 Serial::Serial(QWidget *parent)
 {
     this->device = new QSerialPort();
-    // QMetaObject::connect(this->device, SIGNAL(readyRead()), this, SLOT(readFromPort()));
 }
 
 
@@ -49,53 +48,54 @@ void Serial::readFromPort() {
 
     buffer.append(this->device->readAll());
 
-    int separator = buffer.find("\r\n");
+    // int separator = buffer.find("\r\n");
 
-    while(separator != std::string::npos) {
-        std::string data = buffer.substr(0, separator + 1);
-        buffer.erase(0, separator+2);
-        separator = buffer.find("\r\n");
+    // while(separator != std::string::npos) {
+    //     std::string data = buffer.substr(0, separator + 1);
+    //     buffer.erase(0, separator+2);
+    //     separator = buffer.find("\r\n");
 
-        int packetSeparator = data.find(";");
+        int packetSeparator = buffer.find(";");
 
         while (packetSeparator != std::string::npos) {
-            std::string packet = data.substr(0, packetSeparator);
-            data.erase(0, packetSeparator+1);
-            packetSeparator = data.find(";");
+            std::string packet = buffer.substr(0, packetSeparator);
+            buffer.erase(0, packetSeparator+1);
+            packetSeparator = buffer.find(";");
 
             char type = packet.at(0);
 
             switch (type)
             {
-            case 'C': {
-                std::string cpu = packet.substr(1, packet.size()-1);
-                emit cpuSpeedChanged(std::atoi(cpu.c_str()));
+            case 'C': {  // get fan1 speed
+                packet.erase(0, 1);
+                emit cpuSpeedChanged(std::atoi(packet.c_str()));
                 break;
             }
 
-            case 'G': {
-                std::string gpu = packet.substr(1, packet.size()-1);
-                emit gpuSpeedChanged(std::atoi(gpu.c_str()));
+            case 'G': {  // get fan2 speed
+                packet.erase(0, 1);
+                emit gpuSpeedChanged(std::atoi(packet.c_str()));
                 break;
             }
 
-            case 'S': {
-                std::string cpu = packet.substr(1, packet.size()-1);
-                emit cpuPercentageChanged(std::atoi(cpu.c_str()));
+            case 'S': {  // get fan1 percentage
+                packet.erase(0, 1);
+                emit cpuPercentageChanged(std::atoi(packet.c_str()));
                 break;
             }
 
-            case 'T': {
-                std::string gpu = packet.substr(1, packet.size()-1);
-                emit gpuPercentageChanged(std::atoi(gpu.c_str()));
+            case 'T': {  // get fan2 percentage
+                packet.erase(0, 1);
+                emit gpuPercentageChanged(std::atoi(packet.c_str()));
                 break;
             }
             
             default:
+                printf("Unkown packet! Size %d, Data: %s\n", packet.size(), packet.c_str());
                 break;
             }
         }
-    }
+    // }
 }
 
 
